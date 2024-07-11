5e430f3b1caebff986da7ae35dfac4cd5219b415
Mihail Makeychev <mikhail@ilartech.ru>
2024-07-11 18:36:59 +0300

    fix

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Libs
+import 'focus-visible'
+
+// Utils
+import { fixFullHeight } from './utils/fix-fullheight'
+
+// Components
+
+document.addEventListener('DOMContentLoaded', function () {
+  fixFullHeight()
+})
+
+// For Backend
+// window.GraphModal = new GraphModal()
